abd591b10b2f0a162a58fc97022dfa53a9898187
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2022-10-27 13:02:41 +0200

    Repo, Logic

--- L3AQTN_HFT_202231.Logic/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace L3AQTN_HFT_202231.Logic
-{
-    public class Class1
-    {
-    }
-}
-
--- L3AQTN_HFT_202231.Repository/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace L3AQTN_HFT_202231.Repository
-{
-    public class Class1
-    {
-    }
-}
-
