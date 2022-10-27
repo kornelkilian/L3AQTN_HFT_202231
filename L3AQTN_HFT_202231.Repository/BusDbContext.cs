9caee1fd1521cc67dbb42ee0cb17889861fdf22e
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2022-10-27 12:19:10 +0200

    DbContext

--- L3AQTN_HFT_202231.Repository/BusDbContext.cs
+++ L3AQTN_HFT_202231.Repository/BusDbContext.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using L3AQTN_HFT_202231.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection.Emit;
+using System.Runtime.ConstrainedExecution;
+
 namespace L3AQTN_HFT_202231.Repository
 {
-	public class BusDbContext
+	public class BusDbContext : DbContext
 	{
-		public BusDbContext()
-		{
-		}
+        public DbSet<Bus> Buses { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        private object DbCreationLock = new object();
+        public BusDbContext()
+        {
+            lock (DbCreationLock)
+            {
+                this.Database.EnsureCreated();
+            }
+        }
+
+        public BusDbContext(DbContextOptions<BusDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+               // optionsBuilder
+                    //.UseLazyLoadingProxies()
+                   // .UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarDb.mdf;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var brand = new Brand() { Id = 1, Name = "Citrom" };
+            var bus = new Bus() { Id = 1, BrandId = 1, Model = "C4", Price = 1000 };
+            var bus2 = new Bus() { Id = 2, BrandId = 1, Model = "C5", Price = 1200 };
+            var owner = new Owner() { Id = 1, Name = "Gyula" };
+
+            modelBuilder.Entity<Bus>(entity =>
+                            entity.HasOne(bus => bus.Brand)
+                            .WithMany(brand => brand.Buses)
+                            .HasForeignKey(bus => bus.BrandId)
+                            .OnDelete(DeleteBehavior.ClientSetNull));
+
+            modelBuilder.Entity<Brand>().HasData(brand);
+            modelBuilder.Entity<Bus>().HasData(bus, bus2);
+            modelBuilder.Entity<Owner>().HasData(owner);
+        }
+
+       
+
 	}
 }
 
