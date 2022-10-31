2586a872e30b96331b7233c3bc94a8caeb81adc5
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2022-10-31 14:03:59 +0100

    repo

---
+++ L3AQTN_HFT_202231.Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using L3AQTN_HFT_202231.Models;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace L3AQTN_HFT_202231.Repo
+{
+    public interface IRepository<T> where T : class, IDbEntity
+    {
+        void Create(T entity);
+        void Delete(int i);
+        T Read(int i);
+        IEnumerable<T> ReadAll();
+
+        bool Update(T item);
+    }
+}
---
+++ L3AQTN_HFT_202231.Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿
+using L3AQTN_HFT_202231.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3AQTN_HFT_202231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class, IDbEntity
+    {
+        private readonly BusDbContext context;
+
+        public Repository(BusDbContext context)
+        {
+            this.context = context;
+        }
+
+        public abstract IEnumerable<T> ReadAll();
+
+
+        public T Read(int i)
+        {
+            return ReadAll().FirstOrDefault(_ => _.Id == i);
+        }
+
+        public void Create(T entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int i)
+        {
+            var item = Read(i);
+            context.Remove(item);
+            context.SaveChanges();
+        }
+
+        public abstract bool Update(T item);
+
+    }
+}
+
