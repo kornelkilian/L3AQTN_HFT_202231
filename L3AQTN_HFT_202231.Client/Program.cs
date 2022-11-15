2c4a79610749287f400a990e30ef13f19e20e656
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2022-11-15 14:05:19 +0100

    CRUD Testing

--- L3AQTN_HFT_202231.Client/Program.cs
+++ L3AQTN_HFT_202231.Client/Program.cs
@@ -1,5 +1,8 @@
-﻿using L3AQTN_HFT_202231.Repository;
+﻿using L3AQTN_HFT_202231.Models;
+using L3AQTN_HFT_202231.Repo;
+using L3AQTN_HFT_202231.Repository;
 using System;
+using System.Linq;
 
 namespace L3AQTN_HFT_202231.Client
 {
@@ -8,7 +11,26 @@ namespace L3AQTN_HFT_202231.Client
         static void Main(string[] args)
         {
             BusDbContext context = new BusDbContext();
-            var buses = context.Buses.AsQueryable();    
+            var buses = context.Buses.AsQueryable();
+
+
+            var b = context.Buses.Where(t => t.Brand.Name.Equals("Citrom"));
+
+
+            IRepository<Bus> repo = new BusRepository(new BusDbContext());
+
+            Bus c = new Bus()
+            {
+                Model = "BMW"
+            };
+            repo.Create(c);
+
+            var another = repo.Read(1);
+            another.Model = "Mercedes";
+            repo.Update(another);
+
+            var items = repo.ReadAll().ToArray();
+            
         }
     }
 }
