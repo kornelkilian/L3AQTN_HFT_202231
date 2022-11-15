2d9deac2b425a6af073dcac3d0a7be2446ef4b3d
Kilian Kornel <kilianivan@gmail.com>
2022-11-15 19:40:28 +0100

    GetAverageByBrand_CallWithMockedRepo_ReturnsAverage
    Added the Test named:
    GetAverageByBrand_CallWithMockedRepo_ReturnsAverage

---
+++ L3AQTN_HFT_202231.Logic/AvaragePriceByBrand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace L3AQTN_HFT_202231.Logic
+{
+        public struct AveragePriceByBrand
+        {
+            public int BrandId { get; set; }
+            public double AveragePrice { get; set; }
+
+            public override string ToString()
+            {
+                return $"BrandId: {BrandId} | AveragePrice : {AveragePrice}";
+            }
+        }
+    }
+
+
+
--- L3AQTN_HFT_202231.Logic/BusLogic.cs
+++ L3AQTN_HFT_202231.Logic/BusLogic.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using L3AQTN_HFT_202231.Models;
+using L3AQTN_HFT_202231.Repo;
 
 namespace L3AQTN_HFT_202231.Logic
 {
-    internal class BusLogic
+    public class BusLogic : IBusLogic
     {
+        private readonly IRepository<Bus> busRepo;
+        private readonly IRepository<Brand> brandRepo;
+
+        public BusLogic(IRepository<Bus> busRepo, IRepository<Brand> brandRepo)
+        {
+            this.busRepo = busRepo;
+            this.brandRepo = brandRepo;
+        }
+
+        public IEnumerable<Bus> GetAllCars()
+        {
+            return busRepo.ReadAll();
+        }
+        public IEnumerable<AveragePriceByBrand> GetAverageByBrand()
+        {
+            var result =
+                GetAllCars().GroupBy(_ => _.BrandId)
+                    .Select(group => new AveragePriceByBrand()
+                    {
+
+                        BrandId = group.Key,
+                        AveragePrice = group.Average(_ => _.Price).Value
+                    });
+
+            return result;
+        }
+
+        public void Update(Bus newItem, int id)
+        {
+            newItem.Id = id;
+            busRepo.Update(newItem);
+        }
+
+        public void Update2(Bus newItem, int id)
+        {
+            var item = busRepo.Read(id);
+
+            item.CopyFrom(newItem);
+
+            busRepo.Update(item);
+        }
     }
 }
