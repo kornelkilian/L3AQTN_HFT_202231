6edca46404bb7d1b4887426ed062a791ca1a5dc4
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2023-05-02 17:45:12 +0200

    Added RestCollection and SignalR

---
+++ L3AQTN_HFT202231.Endpoint/Services/SignalRHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace L3AQTN_HFT202231.Endpoint.Services
+{
+    public class SignalRHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Clients.Caller.SendAsync("Connected", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.Caller.SendAsync("Disconnected", Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+
+}
