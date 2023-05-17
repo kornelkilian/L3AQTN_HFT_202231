34f67da19986c4730552f0e322bb5f6dcbe51d07
Kilian Kornel <77408636+kornelkilian@users.noreply.github.com>
2023-05-17 12:36:42 +0200

    PUT support for JSClient

--- L3AQTN_HFT_202231.JSClient/wwwroot/script.js
+++ L3AQTN_HFT_202231.JSClient/wwwroot/script.js
@@ -2,9 +2,11 @@
     .then(x => x.json())
     .then(y => console.log(y));
 
+document.getElementById('updateformdiv').style.display = 'none';
 
 let buses = [];
 let connection = null;
+let busIdToUpdate = -1;
 getdata();
 setupSignalR();
 
@@ -21,6 +23,9 @@ function setupSignalR() {
     });
 
     connection.on("BusDeleted", (user, message) => {
+        getdata();
+    });
+    connection.on("BusUpdated", (user, message) => {
         getdata();
     });
 
@@ -53,15 +58,57 @@ async function getdata() {
 }
 
 function display() {
+
     document.getElementById('resultarea').innerHTML = "";
     buses.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>"
             + t.model + "</td><td>" +
-            `<button type="button" onclick="remove(${t.id})">Delete</button>`
+        `<button type="button" onclick="remove(${t.id})">Delete</button>` +
+        `<button type="button" onclick="showupdate(${t.id})">Update</button>`
+
             + "</td></tr>";
     });
 }
+
+function showupdate(id) {
+    document.getElementById('modelnametoupdate').value = buses.find(t => t['id'] == id)['model'];
+    document.getElementById('pricetoupdate').value = buses.find(t => t['id'] == id)['price'];
+    document.getElementById('brandidtoupdate').value = buses.find(t => t['id'] == id)['brandId'];
+    document.getElementById('owneridtoupdate').value = buses.find(t => t['id'] == id)['ownerId'];
+
+    document.getElementById('updateformdiv').style.display = 'flex';
+    busIdToUpdate = id;
+}
+
+function update() {
+    document.getElementById('updateformdiv').style.display = 'none';
+    let name = document.getElementById('modelnametoupdate').value;
+    let price_ = document.getElementById('pricetoupdate').value;
+    let brand = document.getElementById('brandidtoupdate').value;
+    let owner = document.getElementById('owneridtoupdate').value;
+
+    fetch('http://localhost:10615/bus', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                id:busIdToUpdate,
+                model: name,
+                price: price_,
+                ownerId: owner,
+                brandId: brand,
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
 
 function remove(id) {
     fetch('http://localhost:10615/bus/' + id, {
